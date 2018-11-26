17dd0bf38baf74d7d426ca09744cca2ea1b86e38
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2018-11-26 22:46:38 +0100

    Make creation of master actors synchronous

--- src/main/java/de/hpi/octopus/OctopusMaster.java
+++ src/main/java/de/hpi/octopus/OctopusMaster.java
@@ -26,30 +26,17 @@ public class OctopusMaster extends OctopusSystem {
 		final Config config = createConfiguration(actorSystemName, MASTER_ROLE, host, port, host, port);
 		
 		final ActorSystem system = createSystem(actorSystemName, config);
-		
-		Cluster.get(system).registerOnMemberUp(new Runnable() {
-			@Override
-			public void run() {
-				system.actorOf(Reaper.props(), Reaper.DEFAULT_NAME);
-				system.actorOf(ClusterListener.props(), ClusterListener.DEFAULT_NAME);
-			//	system.actorOf(MetricsListener.props(), MetricsListener.DEFAULT_NAME);
 
-				system.actorOf(Profiler.props(), Profiler.DEFAULT_NAME);
-				
-				for (int i = 0; i < workers; i++)
-					system.actorOf(Worker.props(), Worker.DEFAULT_NAME + i);
-				
-			//	int maxInstancesPerNode = workers; // TODO: Every node gets the same number of workers, so it cannot be a parameter for the slave nodes
-			//	Set<String> useRoles = new HashSet<>(Arrays.asList("master", "slave"));
-			//	ActorRef router = system.actorOf(
-			//		new ClusterRouterPool(
-			//			new AdaptiveLoadBalancingPool(SystemLoadAverageMetricsSelector.getInstance(), 0),
-			//			new ClusterRouterPoolSettings(10000, workers, true, new HashSet<>(Arrays.asList("master", "slave"))))
-			//		.props(Props.create(Worker.class)), "router");
-			}
-		});
+        system.actorOf(Reaper.props(), Reaper.DEFAULT_NAME);
+        system.actorOf(ClusterListener.props(), ClusterListener.DEFAULT_NAME);
+        //	system.actorOf(MetricsListener.props(), MetricsListener.DEFAULT_NAME);
 
-		try {
+        system.actorOf(Profiler.props(), Profiler.DEFAULT_NAME);
+
+        for (int i = 0; i < workers; i++)
+            system.actorOf(Worker.props(), Worker.DEFAULT_NAME + i);
+
+        try {
 			system.actorSelection("/user/" + Profiler.DEFAULT_NAME).tell(new Profiler.TaskMessage(readCSV(filepath), nrSlaves), ActorRef.noSender());
 		} catch (IOException e) {
 			e.printStackTrace();
